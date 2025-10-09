9ac654f8600adefad1fe7e51678ac3111f6f5be9
Mairene-Villasmil <mairenevillasmilf@gmial.com>
2025-10-09 11:54:56 -0300

    gestion admin

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-base: process.env.VITE_BASE || '/', 
+ base: '/LazarilloApp/',
   plugins: [react()],
   server: {
     proxy: {
